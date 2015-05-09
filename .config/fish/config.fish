b79bf8f52fbdb4123a1b49f32fb4d2c39d9b9104
Mitchell Kember <mk12360@gmail.com>
2015-05-09 12:30:24 -0400

    Remove double tabs in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,29 @@
 # Set the prompt.
 function fish_prompt --description "Write out the prompt"
-		echo -n "$USER:"
-		set_color $fish_color_cwd
-		echo -ns (prompt_pwd)
-		set_color normal
-		echo -ns '> '
+	echo -n "$USER:"
+	set_color $fish_color_cwd
+	echo -ns (prompt_pwd)
+	set_color normal
+	echo -ns '> '
 end
 
 # Shortcuts
 function sz --description "Calculate the size of a directory"
-		command du -sh $argv
+	command du -sh $argv
 end
 function upd --description "Updates homebrew packages"
-		command brew update
-		and command brew upgrade
+	command brew update
+	and command brew upgrade
 end
 function blog --description "Starts the Hugo server for my blog"
-		cd ~/icloud/blog
-		and command hugo server -w
+	cd ~/icloud/blog
+	and command hugo server -w
 end
 function notif --description "Makes a notification via Notification Center"
-		command terminal-notifier -activate com.apple.Terminal -message "$argv"
+	command terminal-notifier -activate com.apple.Terminal -message "$argv"
 end
 function sucontext --description "Sets up the ConTeXt root"
-		echo "TODO: implement this function"
+	echo "TODO: implement this function"
 end
 
 # Environment variables
