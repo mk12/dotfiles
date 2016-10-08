b09c16ed70e2f83660f8eb18bc44b19fb4fe943c
Mitchell Kember <mk12360@gmail.com>
2016-10-08 15:36:03 -0700

    Dissassemble (dump) function in fish config

--- .config/fish/darwin.fish
+++ .config/fish/darwin.fish
@@ -3,6 +3,10 @@ function upd --description "Updates homebrew packages"
 	and brew upgrade
 end
 
+function dump --description "Dissassemble to Intel syntax"
+	gobjdump -d -M intel -s $argv
+end
+
 function notif --description "Makes a notification via Notification Center"
 	terminal-notifier -activate com.apple.Terminal -message "$argv"
 end
