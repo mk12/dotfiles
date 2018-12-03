ba11fd443ac0325261459622dbae6bee11569e7f
Mitchell Kember <mk12360@gmail.com>
2018-12-02 16:17:25 -0800

    Add script for keeping init.vim/vimrc changes in sync

---
+++ patch_vimrc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -xeufo pipefail
+
+if [[ "$(git status -s uno)" != 'M  .config/nvim/init.vim' ]]; then
+	exit 1
+fi
+
+git diff -- .config/nvim/init.vim > v.patch
+sed -i '' 's#\.config/nvim/init.vim#.vimrc#g' v.patch
+git apply v.patch
+rm v.patch
