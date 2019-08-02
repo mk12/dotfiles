6fcdc1990ba2813e30641dfd0435bfc82b686869
Mitchell Kember <mk12360@gmail.com>
2019-08-01 23:36:33 -0400

    Add some stuff to profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,13 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.21.1"]
-            [io.aviso/pretty "0.1.37"]
-            [lein-kibit "0.1.6"]]
-  :dependencies [[io.aviso/pretty "0.1.37"]]
+ {:plugins [[io.aviso/pretty "0.1.37"]
+            [jonase/eastwood "0.3.5"]
+            [lein-kibit "0.1.7"]]
+  :dependencies [[im.chit/vinyasa.inject "0.4.7"]
+                 [io.aviso/pretty "0.1.37"]
+                 [philoskim/debux "0.5.6"]
+                 [pjstadig/humane-test-output "0.9.0"]]
+  :injections [(require 'vinyasa.inject 'debux.core)
+               (vinyasa.inject/inject '[clojure.core [debux.core dbg dbgn]])
+               (require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
   :middleware [io.aviso.lein-pretty/inject]}}
