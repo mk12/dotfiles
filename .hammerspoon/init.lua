fa0e0516b48189da862424db135ecda5d4d964ac
Mitchell Kember <mk12360@gmail.com>
2019-12-25 12:52:43 -0500

    More revamp progress

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,18 +1,9 @@
--- Install spoons.
-hs.loadSpoon("SpoonInstall")
-spoon.SpoonInstall.repos.default = {
-    url = "https://github.com/mk12/Spoons",
-    desc = "mk12's Hammerspoon Spoon repository",
-    branch = "master",
-}
-spoon.SpoonInstall:andUse("UnsplashRandom")
-
 -- Install the hs binary. This just copies a symlink, so there's no harm in
 -- repeating it every reload. Doing it in setupmacos.sh is too complicated.
 hs.ipc.cliInstall()
 
--- Send a notification saying the bell rang in kitty.
-function kittyNotifyBell()
+-- Sends a notification saying the bell rang in kitty.
+function sendKittyBellNotification()
     hs.notify.new(function()
         hs.application.launchOrFocus("kitty")
     end, {
@@ -21,7 +12,7 @@ function kittyNotifyBell()
     }):send()
 end
 
--- Return the hotkitty app if it is currently running.
+-- Returns the hotkitty app if it is currently running.
 local function getHotkitty()
     if not (hotkitty and hotkitty:isRunning()) then
         hotkitty = nil
@@ -35,7 +26,7 @@ local function getHotkitty()
     return hotkitty
 end
 
--- Show/hide the hotkitty window.
+-- Hotkey to show/hide the hotkitty window.
 hs.hotkey.bind({"ctrl"}, "space", function()
     local app = getHotkitty()
     if app then
@@ -51,7 +42,25 @@ hs.hotkey.bind({"ctrl"}, "space", function()
     end
 end)
 
--- TODO super?
-hs.hotkey.bind({"cmd", "option", "ctrl"}, "J", function()
+-- Hyper key for global shortcuts.
+local hyper = {"cmd", "option", "ctrl"}
+
+-- Returns a function to bind to a hyper shortcut.
+local function hyperApp(name)
+    return function()
+        local app = hs.application.get(name)
+        if app and app:mainWindow() then
+            if app:isFrontmost() then
+                app:hide()
+            else
+                app:activate()
+            end
+        else
+            hs.application.launchOrFocus(name)
+        end
+    end
+end
+
+hs.hotkey.bind(hyper, "J", function()
     os.execute("open ~/ia/Journal/Today.txt")
 end)
